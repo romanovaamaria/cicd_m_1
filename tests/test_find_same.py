f1d7470f84c2b6824297fa5e5abe2efe1baf40fc
Mariia Romanova <romanovamr0409@icloud.com>
2023-03-28 13:51:00 +0300

    report and requirements

--- tests/test_find_same.py
+++ tests/test_find_same.py
@@ -4,7 +4,7 @@ import pytest
 def test_check_if_same_while_same():
     file_list1 = ["1\n", "2\n"]
     file_list2 = ["2\n", "1\n",]
-    expected_output = ["2\n", "1\n"]
+    expected_output = ["1\n", "2\n"]
     find_same(file_list1, file_list2)
     with open('same.txt') as f:
         assert f.readlines() == expected_output
